c54741d590caccb24e140dec0d435786f880b025
Jeremy Drake <github@jdrake.com>
2017-06-08 23:35:25 -0700

    add config 'module' to be imported in scripts intended for use from the command line rather than from within appengine

---
+++ cmdline_config.py
@@ -0,0 +1,5 @@
+import sys, os
+# Add any libraries installed in the "lib" folder.
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+# Add python-pgpdump git submodule
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib', 'python-pgpdump'))
