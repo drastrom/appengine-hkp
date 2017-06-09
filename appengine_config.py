42d0ce9cb6521211c73ded2a442162e69a53e83c
Jeremy Drake <github@jdrake.com>
2017-06-08 22:31:26 -0700

    add python-pgpdump dir to vendor path in appengine_config.py

---
+++ appengine_config.py
@@ -0,0 +1,13 @@
+from google.appengine.ext import vendor
+import os
+
+# Add any libraries installed in the "lib" folder.
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+# Add python-pgpdump git submodule
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib', 'python-pgpdump'))
+
+#def webapp_add_wsgi_middleware(app):
+#	from google.appengine.ext.appstats import recording
+#	app = recording.appstats_wsgi_middleware(app)
+#	return app
+
