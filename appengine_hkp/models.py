420454d9f7720ef0bd64faf82334b8e05a6938e8
Jeremy Drake <github@jdrake.com>
2017-06-09 19:10:26 -0700

    Flesh out models and fix, and add a test script.
    The fixes to models were a ; where a : should have been, and forgetting
    to import re.  Fleshing out included using repeated KeyProperties for
    uids and subkeys, since I decided I'll need to use the ancestor
    relationships for those.  Also added python properties for long and
    short key ids computed from the stored reversed_fingerprint (needed to
    query suffixes).
    
    The test script replicates the models as normal python objects, and
    uses the pgpdump library to populate them.

--- appengine_hkp/models.py
+++ appengine_hkp/models.py
@@ -2,11 +2,23 @@
 
 from google.appengine.ext import ndb
 from google.appengine.ext.ndb import polymodel
+import codecs
+import re
 
 class Uid(ndb.Model):
-	name = ndb.StringProperty('n', indexed=True)
-	comment = ndb.StringProperty('c', indexed=False)
-	email = ndb.StringProperty('e', indexed=True)
+	uid = ndb.StringProperty('u', indexed=True, required=True)
+
+	_uid_regex = re.compile(r'^([^<(]+)? ?(?:\(([^\)]*)\))? ?<([^>]*)>?')
+	def _parse_uid(self):
+		match = self._uid_regex.match(self.uid)
+		if match:
+			return (match.group(1).strip() if match.group(1) is not None else None, match.group(2).strip() if match.group(2) is not None else None, match.group(3).strip() if match.group(3) is not None else None)
+		else:
+			return (None, None, None)
+
+	name = ndb.ComputedProperty(lambda self: self._parse_uid()[0], 'n', indexed=True)
+	comment = ndb.ComputedProperty(lambda self: self._parse_uid()[1], 'c', indexed=True)
+	email = ndb.ComputedProperty(lambda self: self._parse_uid()[2], 'e', indexed=True)
 
 class KeyBase(polymodel.PolyModel):
 	reversed_fingerprint = ndb.BlobProperty('rfp', indexed=True, required=True)
@@ -16,10 +28,27 @@ class KeyBase(polymodel.PolyModel):
 	algorithm_type = ndb.StringProperty('a', indexed=False, required=True)
 	bitlen = ndb.IntegerProperty('b', indexed=False)
 
-class PublicKey(KeyBase);
-	uid = ndb.StructuredProperty(Uid, 'u', indexed=True, repeated=True)
-	key_data = ndb.BlobProperty('d', indexed=False, required=True)
+	@property
+	def fingerprint(self):
+		return codecs.encode(self.reversed_fingerprint[::-1], 'hex').upper().encode('ascii')
+
+	def _fingerprint_suffix(self, bytelen):
+		return codecs.encode(self.reversed_fingerprint[bytelen-1::-1], 'hex').upper().encode('ascii')
+
+	@property
+	def keyid(self):
+		return self._fingerprint_suffix(8)
+
+	@property
+	def shortkeyid(self):
+		return self._fingerprint_suffix(4)
+
 
 class PublicSubkey(KeyBase):
 	pass
+
+class PublicKey(KeyBase):
+	uids = ndb.KeyProperty(Uid, 'u', indexed=True, repeated=True)
+	subkeys = ndb.KeyProperty(PublicSubkey, 's', indexed=True, repeated=True)
+	key_data = ndb.BlobProperty('d', indexed=False, required=True)
 
---
+++ test.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+	import cmdline_config
+
+import pgpdump
+import pgpdump.packet
+import pprint
+
+pp = pprint.PrettyPrinter()
+
+import datetime
+import re
+import codecs
+
+class Uid(object):
+	def __repr__(self):
+		return "<%s: %r>" % (self.__class__.__name__, self.__dict__)
+
+	_uid_regex = re.compile(r'^([^<(]+)? ?(?:\(([^\)]*)\))? ?<([^>]*)>?')
+
+	def __init__(self):
+		self.uid = ""
+
+	@property
+	def name(self):
+		return self._parse_uid()[0]
+
+	@property
+	def comment(self):
+		return self._parse_uid()[1]
+
+	@property
+	def email(self):
+		return self._parse_uid()[2]
+
+	def _parse_uid(self):
+		match = self._uid_regex.match(self.uid)
+		if match:
+			return (match.group(1).strip() if match.group(1) is not None else None, match.group(2).strip() if match.group(2) is not None else None, match.group(3).strip() if match.group(3) is not None else None)
+		else:
+			return (None, None, None)
+
+
+class KeyBase(object):
+	def __repr__(self):
+		return "<%s: %r>" % (self.__class__.__name__, self.__dict__)
+
+	def __init__(self):
+		self.reversed_fingerprint = bytearray()
+		self.creation_time = datetime.datetime.utcfromtimestamp(0)
+		self.expiration_time = None
+		self.flags = 0
+		algorithm_type = ""
+		bitlen = 0
+
+	@property
+	def fingerprint(self):
+		return codecs.encode(self.reversed_fingerprint[::-1], 'hex').upper().encode('ascii')
+
+	def fingerprint_suffix(self, bytelen):
+		return codecs.encode(self.reversed_fingerprint[bytelen-1::-1], 'hex').upper().encode('ascii')
+
+	@property
+	def keyid(self):
+		return self.fingerprint_suffix(8)
+
+	@property
+	def shortkeyid(self):
+		return self.fingerprint_suffix(4)
+
+class PublicSubkey(KeyBase):
+	pass
+
+class PublicKey(KeyBase):
+	def __init__(self):
+		super(PublicKey, self).__init__()
+		self.uids = []
+		self.subkeys = []
+		self.key_data = bytearray()
+
+
+with open('mykey.asc', 'rb') as infile:
+	data = pgpdump.AsciiData(infile.read())
+
+pubkeys = []
+pubkey = None
+curkey = None
+
+for packet in data.packets():
+	if isinstance(packet, pgpdump.packet.PublicKeyPacket) and not isinstance(packet, pgpdump.packet.SecretKeyPacket):
+		if packet.name == "Public Key Packet":
+			pubkey = PublicKey()
+			pubkeys.append(pubkey)
+			curkey = pubkey
+			pubkey.key_data = data.data
+		else:
+			curkey = PublicSubkey()
+			pubkey.subkeys.append(curkey)
+
+		curkey.reversed_fingerprint = codecs.decode(packet.fingerprint.decode('ascii'), 'hex')[::-1]
+		curkey.creation_time = packet.creation_time
+		curkey.expiration_time = packet.expiration_time
+		curkey.algorithm_type = packet.pub_algorithm_type
+		curkey.bitlen = packet.modulus_bitlen
+	elif isinstance(packet, pgpdump.packet.UserIDPacket):
+		curuid = Uid()
+		pubkey.uids.append(curuid)
+		curuid.uid = packet.user
+
+pp.pprint([pubkey.__dict__ for pubkey in pubkeys])
+print "Real fingerprints"
+pp.pprint([pubkey.fingerprint for pubkey in pubkeys])
+pp.pprint([pubkey.keyid for pubkey in pubkeys])
+pp.pprint([pubkey.shortkeyid for pubkey in pubkeys])
