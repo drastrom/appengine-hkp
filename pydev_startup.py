3bafd830fad561d7e7b2c7b44b2ce756b1128602
Jeremy Drake <github@jdrake.com>
2017-06-21 23:26:17 -0700

    add pydev_startup.py to project, clean up pydev_startup.py a little

--- pydev_startup.py
+++ pydev_startup.py
@@ -1,11 +1,13 @@
-import sys
-import os
 import ptvsd
+
 import getpass
 import platform
+#Override methods that die in appengine sandbox
 getpass.getuser = lambda: "debugger"
 platform.system = lambda: "Windows"
-#Feel free to change the secret and port number
-ptvsd.enable_attach(secret = 'gae', address = ('0.0.0.0', 3000))
-#The debug server has started and you can now use VS Code to attach to the application for debugging
+
+#In visual studio attach dialog, you'll need to select transport python remote,
+#and qualifier tcp://gae@localhost:3000
+ptvsd.enable_attach(secret = 'gae', address = ('127.0.0.1', 3000))
+#The debug server has started and you can now attach to the application for debugging
 print("Google App Engine has started, ready to attach the debugger")
